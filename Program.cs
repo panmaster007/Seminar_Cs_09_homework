a230aa10777487b6785cc747b04a544f59e481be
Igor <fullstack@naladka.su>
2023-03-01 13:43:26 +0300

    init commit

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿void TaskMenu()
+{
+	bool exitOut = true;
+
+	while (exitOut == true)
+	{
+		Console.WriteLine("Введите номер задачи(1, 2, либо q/Q для выхода):");
+		string selectTask = "";
+		selectTask = Console.ReadLine();
+
+		switch (selectTask)
+		{
+			case "1":
+				{
+					Console.Clear();
+					Task_1();
+					break;
+				}
+
+			case "2":
+				{
+					Console.Clear();
+					Task_2();
+					break;
+				}
+
+			case "q":
+				{
+					exitOut = false;
+					Console.Clear();
+					break;
+				}
+
+			case "Q":
+				{
+					exitOut = false;
+					Console.Clear();
+					break;
+				}
+
+			default:
+				{
+					Console.WriteLine("Вы не попали в нужную кнопку, попробуйте ещё раз!");
+					break;
+				}
+		}
+	}
+}
+
+// здесь располагаем методы для выполнения задач
+
+void Task_1()
+{
+	// Здесь вызываем необходимые методы для выполнения 1-й задачи
+}
+
+void Task_2()
+{
+	// Здесь вызываем необходимые методы для выполнения 2-й задачи
+}
+
+TaskMenu();
